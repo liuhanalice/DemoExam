14485503b0e01c8a559352f1fc026e9927a50c0b
Han Liu <hl3608@columbia.edu>
2023-03-29 16:35:48 -0400

    section 0 static template

---
+++ scripts/section0.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    $("#panelToggle").click(function() {
+      if ($(this).hasClass("collapsed")) {
+        $(this).html('<i class="bi bi-chevron-compact-down"></i>');
+      } else {
+        $(this).html('<i class="bi bi-chevron-compact-up"></i>');
+      }
+    });
+});
